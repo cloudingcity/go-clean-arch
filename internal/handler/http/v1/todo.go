46fb17cc8ee727f3d8b40cb830d0dafe5a03555d
clouding <ghost0436@gmail.com>
2025-12-02 23:14:16 +0800

    feat: implement other API endpoints

--- internal/handler/http/v1/todo.go
+++ internal/handler/http/v1/todo.go
@@ -1,9 +1,11 @@
 package v1
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
+	"github.com/cloudingcity/todo/internal/entity"
 	"github.com/cloudingcity/todo/internal/service"
 	"github.com/gin-gonic/gin"
 )
@@ -17,6 +19,10 @@ func NewTodoRoutes(rg *gin.RouterGroup, srv service.Todo) {
 		srv: srv,
 	}
 	rg.POST("/todos", h.create)
+	rg.GET("/todos", h.list)
+	rg.GET("/todos/:id", h.get)
+	rg.PATCH("/todos/:id", h.update)
+	rg.DELETE("/todos/:id", h.remove)
 }
 
 type createTodoReq struct {
@@ -55,4 +61,129 @@ func (h *todoHandler) create(c *gin.Context) {
 		CreatedAt:   todo.CreatedAt,
 		UpdatedAt:   todo.UpdatedAt,
 	})
+}
+
+type listTodoResp struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	IsCompleted bool      `json:"isCompleted"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+func (h *todoHandler) list(c *gin.Context) {
+	todos, err := h.srv.List()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	resp := make([]listTodoResp, len(todos))
+	for i, todo := range todos {
+		resp[i] = listTodoResp{
+			ID:          todo.ID,
+			Title:       todo.Title,
+			Description: todo.Description,
+			IsCompleted: todo.IsCompleted,
+			CreatedAt:   todo.CreatedAt,
+			UpdatedAt:   todo.UpdatedAt,
+		}
+	}
+	c.JSON(http.StatusOK, resp)
+}
+
+type getTodoReq struct {
+	ID int `uri:"id" binding:"required"`
+}
+
+type getTodoResp struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	IsCompleted bool      `json:"isCompleted"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+func (h *todoHandler) get(c *gin.Context) {
+	var req getTodoReq
+	if err := c.ShouldBindUri(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	todo, err := h.srv.Get(req.ID)
+	if errors.Is(err, service.ErrNotFound) {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, getTodoResp{
+		ID:          todo.ID,
+		Title:       todo.Title,
+		Description: todo.Description,
+		IsCompleted: todo.IsCompleted,
+		CreatedAt:   todo.CreatedAt,
+		UpdatedAt:   todo.UpdatedAt,
+	})
+}
+
+type updateTodoReq struct {
+	ID          int     `uri:"id" binding:"required"`
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+	IsCompleted *bool   `json:"isCompleted"`
+}
+
+func (h *todoHandler) update(c *gin.Context) {
+	var req updateTodoReq
+	if err := c.ShouldBindUri(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := c.ShouldBind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	input := entity.UpdateTodoInput{
+		Title:       req.Title,
+		Description: req.Description,
+		IsCompleted: req.IsCompleted,
+	}
+	if err := h.srv.Update(req.ID, input); errors.Is(err, service.ErrNotFound) {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
+
+type removeTodoReq struct {
+	ID int `uri:"id" binding:"required"`
+}
+
+func (h *todoHandler) remove(c *gin.Context) {
+	var req removeTodoReq
+	if err := c.ShouldBindUri(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := h.srv.Delete(req.ID); errors.Is(err, service.ErrNotFound) {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
--- internal/handler/http/v1/todo_test.go
+++ internal/handler/http/v1/todo_test.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -9,8 +10,10 @@ import (
 	"time"
 
 	"github.com/cloudingcity/todo/internal/entity"
+	"github.com/cloudingcity/todo/internal/service"
 	"github.com/cloudingcity/todo/internal/service/mocks"
 	"github.com/gin-gonic/gin"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/mock/gomock"
 )
@@ -101,6 +104,342 @@ func (s *todoSuite) TestCreate() {
 
 			s.Equal(tt.wantCode, w.Code)
 			s.JSONEq(tt.wantResp, w.Body.String())
+		})
+	}
+}
+
+func (s *todoSuite) TestList() {
+	tests := []struct {
+		desc     string
+		mock     func()
+		wantCode int
+		wantResp string
+	}{
+		{
+			desc: "success",
+			mock: func() {
+				s.mockSrv.EXPECT().List().Return([]entity.Todo{
+					{
+						ID:          1,
+						Title:       "title-1",
+						Description: "desc-1",
+						IsCompleted: false,
+						CreatedAt:   time.Unix(123456789, 0),
+						UpdatedAt:   time.Unix(123456789, 0),
+					},
+					{
+						ID:          2,
+						Title:       "title-2",
+						Description: "desc-2",
+						IsCompleted: true,
+						CreatedAt:   time.Unix(123456789, 0),
+						UpdatedAt:   time.Unix(123456789, 0),
+					},
+					{
+						ID:          3,
+						Title:       "title-3",
+						Description: "desc-3",
+						IsCompleted: false,
+						CreatedAt:   time.Unix(123456789, 0),
+						UpdatedAt:   time.Unix(123456789, 0),
+					},
+				}, nil).Times(1)
+			},
+			wantCode: http.StatusOK,
+			wantResp: `[
+				{
+				  "id": 1,
+				  "title": "title-1",
+				  "description": "desc-1",
+				  "isCompleted": false,
+				  "createdAt": "1973-11-30T05:33:09+08:00",
+				  "updatedAt": "1973-11-30T05:33:09+08:00"
+				},
+				{
+				  "id": 2,
+				  "title": "title-2",
+				  "description": "desc-2",
+				  "isCompleted": true,
+				  "createdAt": "1973-11-30T05:33:09+08:00",
+				  "updatedAt": "1973-11-30T05:33:09+08:00"
+				},
+				{
+				  "id": 3,
+				  "title": "title-3",
+				  "description": "desc-3",
+				  "isCompleted": false,
+				  "createdAt": "1973-11-30T05:33:09+08:00",
+				  "updatedAt": "1973-11-30T05:33:09+08:00"
+				}
+            ]`,
+		},
+		{
+			desc: "service list failed",
+			mock: func() {
+				s.mockSrv.EXPECT().List().Return(nil, errors.New("something wrong")).Times(1)
+			},
+			wantCode: http.StatusInternalServerError,
+			wantResp: `{
+				"error": "something wrong"
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		s.Run(tt.desc, func() {
+			if tt.mock != nil {
+				tt.mock()
+			}
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodGet, "/v1/todos", nil)
+			s.router.ServeHTTP(w, req)
+
+			s.Equal(tt.wantCode, w.Code)
+			s.JSONEq(tt.wantResp, w.Body.String())
+		})
+	}
+}
+
+func (s *todoSuite) TestGet() {
+	tests := []struct {
+		desc     string
+		id       string
+		mock     func()
+		wantCode int
+		wantResp string
+	}{
+		{
+			desc: "success",
+			id:   "1",
+			mock: func() {
+				s.mockSrv.EXPECT().Get(1).Return(&entity.Todo{
+					ID:          1,
+					Title:       "title-1",
+					Description: "desc-1",
+					IsCompleted: false,
+					CreatedAt:   time.Unix(123456789, 0),
+					UpdatedAt:   time.Unix(123456789, 0),
+				}, nil).Times(1)
+			},
+			wantCode: http.StatusOK,
+			wantResp: `{
+			  "id": 1,
+			  "title": "title-1",
+			  "description": "desc-1",
+			  "isCompleted": false,
+			  "createdAt": "1973-11-30T05:33:09+08:00",
+			  "updatedAt": "1973-11-30T05:33:09+08:00"
+            }`,
+		},
+		{
+			desc:     "wrong id",
+			id:       "wrong-id",
+			mock:     func() {},
+			wantCode: http.StatusBadRequest,
+			wantResp: `{
+				"error": "strconv.ParseInt: parsing \"wrong-id\": invalid syntax"
+			}`,
+		},
+		{
+			desc: "not found",
+			id:   "1",
+			mock: func() {
+				s.mockSrv.EXPECT().Get(1).Return(nil, service.ErrNotFound).Times(1)
+			},
+			wantCode: http.StatusNotFound,
+			wantResp: `{
+				"error": "not found"
+			}`,
+		},
+		{
+			desc: "service get failed",
+			id:   "1",
+			mock: func() {
+				s.mockSrv.EXPECT().Get(1).Return(nil, errors.New("something wrong")).Times(1)
+			},
+			wantCode: http.StatusInternalServerError,
+			wantResp: `{
+				"error": "something wrong"
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		s.Run(tt.desc, func() {
+			if tt.mock != nil {
+				tt.mock()
+			}
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/v1/todos/%s", tt.id), nil)
+			s.router.ServeHTTP(w, req)
+
+			s.Equal(tt.wantCode, w.Code)
+			s.JSONEq(tt.wantResp, w.Body.String())
+		})
+	}
+}
+
+func (s *todoSuite) TestUpdate() {
+	tests := []struct {
+		desc     string
+		id       string
+		body     string
+		mock     func()
+		wantCode int
+		wantResp string
+	}{
+		{
+			desc: "success",
+			id:   "1",
+			body: `{"title": "title-1", "description": "desc-1", "isCompleted": true}`,
+			mock: func() {
+				s.mockSrv.EXPECT().Update(1, entity.UpdateTodoInput{
+					Title:       lo.ToPtr("title-1"),
+					Description: lo.ToPtr("desc-1"),
+					IsCompleted: lo.ToPtr(true),
+				}).Return(nil).Times(1)
+			},
+			wantCode: http.StatusNoContent,
+			wantResp: "",
+		},
+		{
+			desc: "wrong id",
+			id:   "wrong-id",
+			body: `{"title": "title-1", "description": "desc-1", "isCompleted": true}`,
+			mock: func() {
+			},
+			wantCode: http.StatusBadRequest,
+			wantResp: `{
+				"error": "strconv.ParseInt: parsing \"wrong-id\": invalid syntax"
+			}`,
+		},
+		{
+			desc: "invalid body",
+			id:   "1",
+			body: `{"title": 999, "description": "desc-1", "isCompleted": true}`,
+			mock: func() {
+			},
+			wantCode: http.StatusBadRequest,
+			wantResp: `{
+				"error": "json: cannot unmarshal number into Go struct field updateTodoReq.title of type string"
+			}`,
+		},
+		{
+			desc: "not found",
+			id:   "1",
+			body: `{"title": "title-1", "description": "desc-1", "isCompleted": true}`,
+			mock: func() {
+				s.mockSrv.EXPECT().Update(1, entity.UpdateTodoInput{
+					Title:       lo.ToPtr("title-1"),
+					Description: lo.ToPtr("desc-1"),
+					IsCompleted: lo.ToPtr(true),
+				}).Return(service.ErrNotFound).Times(1)
+			},
+			wantCode: http.StatusNotFound,
+			wantResp: `{
+				"error": "not found"
+			}`,
+		},
+		{
+			desc: "service update failed",
+			id:   "1",
+			body: `{"title": "title-1", "description": "desc-1", "isCompleted": true}`,
+			mock: func() {
+				s.mockSrv.EXPECT().Update(1, entity.UpdateTodoInput{
+					Title:       lo.ToPtr("title-1"),
+					Description: lo.ToPtr("desc-1"),
+					IsCompleted: lo.ToPtr(true),
+				}).Return(errors.New("something wrong")).Times(1)
+			},
+			wantCode: http.StatusInternalServerError,
+			wantResp: `{
+				"error": "something wrong"
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		s.Run(tt.desc, func() {
+			if tt.mock != nil {
+				tt.mock()
+			}
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodPatch, fmt.Sprintf("/v1/todos/%s", tt.id), strings.NewReader(tt.body))
+			req.Header.Set("Content-Type", "application/json")
+			s.router.ServeHTTP(w, req)
+
+			s.Equal(tt.wantCode, w.Code)
+			if tt.wantResp != "" {
+				s.JSONEq(tt.wantResp, w.Body.String())
+			}
+		})
+	}
+}
+
+func (s *todoSuite) TestDelete() {
+	tests := []struct {
+		desc     string
+		id       string
+		mock     func()
+		wantCode int
+		wantResp string
+	}{
+		{
+			desc: "success",
+			id:   "1",
+			mock: func() {
+				s.mockSrv.EXPECT().Delete(1).Return(nil).Times(1)
+			},
+			wantCode: http.StatusNoContent,
+			wantResp: "",
+		},
+		{
+			desc:     "wrong id",
+			id:       "wrong-id",
+			mock:     func() {},
+			wantCode: http.StatusBadRequest,
+			wantResp: `{
+				"error": "strconv.ParseInt: parsing \"wrong-id\": invalid syntax"
+			}`,
+		},
+		{
+			desc: "not found",
+			id:   "1",
+			mock: func() {
+				s.mockSrv.EXPECT().Delete(1).Return(service.ErrNotFound).Times(1)
+			},
+			wantCode: http.StatusNotFound,
+			wantResp: `{
+				"error": "not found"
+			}`,
+		},
+		{
+			desc: "service delete failed",
+			id:   "1",
+			mock: func() {
+				s.mockSrv.EXPECT().Delete(1).Return(errors.New("something wrong")).Times(1)
+			},
+			wantCode: http.StatusInternalServerError,
+			wantResp: `{
+				"error": "something wrong"
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		s.Run(tt.desc, func() {
+			if tt.mock != nil {
+				tt.mock()
+			}
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/v1/todos/%s", tt.id), nil)
+			s.router.ServeHTTP(w, req)
+
+			s.Equal(tt.wantCode, w.Code)
 		})
 	}
 }
